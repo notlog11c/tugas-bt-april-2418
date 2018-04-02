100b6173d65ab53ff2cc8592af7828990eaa7ac2
Rifky <notlog11c>
2018-04-02 14:33:31 +0700

    ini nyoba lagi kok

--- oop.php
+++ oop.php
@@ -16,11 +16,19 @@ class laptop {
 }
 
 $laptop_notlog11c = new laptop();
+$laptop_angkrek = new laptop();
 
+//ini juga cobaaa
 $laptop_notlog11c->pemilik="notlog11c";
 $laptop_notlog11c->merk="HP";
 $laptop_notlog11c->ukuran_layar="15 inchi";
 
+//cobaa
+$laptop_angkrek->pemilik="angkrek";
+$laptop_angkrek->merk="asus";
+$laptop_angkrek->ukuran_layar="22 inchi";
+
+// tampilkan property
 echo $laptop_notlog11c->pemilik;
 echo "<br>";
 echo $laptop_notlog11c->merk;
@@ -29,8 +37,16 @@ echo $laptop_notlog11c->ukuran_layar;
 echo "<br>";
 
 //tampikan method
-echo $laptop_notlog11c->hidupkan_laptop;
+echo $laptop_notlog11c->hidupkan_laptop();
 echo "<br>";
-echo $laptop_notlog11c->matikan_laptop;
+echo $laptop_notlog11c->matikan_laptop();
+echo "<br>";
+
+//tampilkan laptop anggrek
+echo $laptop_angkrek->pemilik;
+echo "<br>";
+echo $laptop_angkrek->merk;
+echo "<br>";
+echo $laptop_angkrek->ukuran_layar;
 
 ?>
